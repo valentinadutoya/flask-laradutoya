efb1d76b818a4eb3c341bf9ee207aa92daf58f2d
valentinadutoya <laravalentinadutoya@gmail.com>
2024-05-15 11:05:37 -0300

    termine lista 2

--- pelis/__init__.py
+++ pelis/__init__.py
@@ -30,3 +30,17 @@ def Actor():
     pagina = render_template('Actor.html', actores=lista_Actor)
 
     return pagina
+
+
+@app.route('/lenguaje')
+def lenguaje():
+    consulta = """
+        SELECT name FROM language 
+        ORDER BY name; 
+    """
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_lenguaje = res.fetchall()
+    pagina = render_template('lenguaje.html', lenguajes=lista_lenguaje) 
+
+    return pagina
