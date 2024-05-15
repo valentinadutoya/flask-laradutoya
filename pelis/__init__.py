b74291735b6888f08271aa81809fa585e6310e99
valentinadutoya <laravalentinadutoya@gmail.com>
2024-05-15 09:59:39 -0300

    termine lista

--- pelis/__init__.py
+++ pelis/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+with app.app_context():
+    from . import db
+    db.init_app(app)
 
 
 @app.route('/')
@@ -12,3 +16,17 @@ def hello():
 @app.route('/bb')
 def bb():
     return 'laamm'
+
+
+@app.route('/actor')
+def Actor():
+    consulta = """
+        SELECT first_name, last_name FROM actor
+        ORDER BY last_name, first_name; 
+    """
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_Actor = res.fetchall()
+    pagina = render_template('Actor.html', actores=lista_Actor)
+
+    return pagina
