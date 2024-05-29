77ff1a3dee0c3849adecf9d388f3b4db68176b4f
valentinadutoya <laravalentinadutoya2019@gmail.com>
2024-05-29 12:02:12 -0300

    falta arreglar lenguaje

--- pelis/actor.py
+++ pelis/actor.py
@@ -6,7 +6,7 @@ bp = Blueprint('actor', __name__,url_prefix='/actor')
 @bp.route('/')
 def Actor():
     consulta = """
-        SELECT first_name, last_name FROM actor
+        SELECT first_name, last_name, actor_id FROM actor
         ORDER BY last_name, first_name; 
     """
     con = db.get_db()
@@ -20,16 +20,23 @@ def Actor():
 def detalle(id):
             con=db.get_db()
             consulta = """
-            SELECT a.first_name,a.last_name, f.title, a.actor_id FROM actor a 
-            JOIN film_actor fa ON a.actor_id = fa.actor_id
-            JOIN film f ON fa.film_id = f.film_id
+            SELECT a.first_name,a.last_name FROM actor a 
             WHERE a.actor_id = ?
-            GROUP BY f.film_id"""
-                
+            """
+
+            consulta2 = """
+            SELECT f.title, f.film_id 
+            FROM film_actor fa JOIN film f ON fa.film_id = f.film_id
+            WHERE fa.actor_id = ?
+            """
+
             resultado= con.execute(consulta,(id,))   
-            actor = {"nombre": resultado[actor],"id" : resultado["a.actor_id"]}
-            detalleActor = resultado["actor"]
-            actor = resultado.fetchone()    
-            pagina = render_template('detalleActor.html', act=actor)
+            actor = resultado.fetchone()
+            
+            resultado= con.execute(consulta2,(id,))   
+            lista_pelis = resultado.fetchall()
+
+            
+            pagina = render_template('detalleActor.html', act=actor, pelis =lista_pelis)
 
             return pagina      
--- pelis/lenguaje.py
+++ pelis/lenguaje.py
@@ -6,7 +6,7 @@ bp = Blueprint('lenguaje', __name__,url_prefix='/lenguaje')
 @bp.route('/')
 def lenguaje():
     consulta = """
-        SELECT name FROM language 
+        SELECT name,languague_id FROM language 
         ORDER BY name; 
     """
     con = db.get_db()
@@ -15,3 +15,28 @@ def lenguaje():
     pagina = render_template('lenguaje.html', lenguajes=lista_lenguaje) 
 
     return pagina
+
+@bp.route('/<int:id>/detalle')
+def detalle(id):
+            con=db.get_db()
+            consulta = """
+            SELECT name FROM language
+            WHERE language_id = 2
+            """
+
+            consulta2 = """
+           SELECT f.title as peliculas, f.film_id FROM film f 
+           JOIN language l on f.language_id = l.language_id
+           WHERE l.language_id = ?
+            """
+
+            resultado= con.execute(consulta,(id,))   
+            lenguajes = resultado.fetchone()
+            
+            resultado= con.execute(consulta2,(id,))   
+            lista_pelis = resultado.fetchall()
+
+            
+            pagina = render_template('detalleLenguaje.html', leng=lenguajes, peli=lista_pelis)
+
+            return pagina      
